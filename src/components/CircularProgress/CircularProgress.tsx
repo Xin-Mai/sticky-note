64da8cb8ea598f901b94347546cbf3d94e046db2
heulwen <heulwen@heulwendeMacBook-Pro.local>
2022-10-25 15:13:11 +0800

    feat: counting down, improve size setting

--- src/components/CircularProgress/CircularProgress.tsx
+++ src/components/CircularProgress/CircularProgress.tsx
@@ -6,48 +6,66 @@ export type Status = 'setting' | 'counting';
 
 interface Props {
   id?: string;
+  initVal?: number;
   size?: number;
   color?: string;
   status?: Status;
   style?: React.CSSProperties,
   onStatusChange?(status: Status): void;
+  onInitValChange?(val: number): void;
 }
 
 export default function CircularProgress(props: Props) {
   const { id = 'circular-progress', size = 200, color = '#61dafb', status = 'setting' } = props;
+  const slotId = id + '-slot';
   const lineWidth = size / 40;
-  const [initVal, setInitVal] = useState(0);
+  const [initVal, setInitVal] = useState(props.initVal || 0);
   const [canvasCtx, setCanvasCtx] = useState<null | CanvasRenderingContext2D>(null);
+  const [slotCtx, setSlotCtx]  = useState<null | CanvasRenderingContext2D>(null);
 
   const slotPath = useRef<null | Path2D>(null);
   const buttonPath = useRef<null | Path2D>(null);
   const canvas = useRef<null | HTMLCanvasElement>(null);
+  const slotCanvas = useRef<null | HTMLCanvasElement>(null);
   const dragging = useRef<boolean>(false);
 
   // get canvas element
   useEffect(() => {
     canvas.current = document.getElementById(id) as HTMLCanvasElement;
-    return () => {canvas.current = null };
+    slotCanvas.current = document.getElementById(slotId) as HTMLCanvasElement;
+    return () => {
+      canvas.current = null;
+      slotCanvas.current = null;
+    };
   });
 
   // init context
   useEffect(() => {
-    if (!canvas.current) return;
+    if (!canvas.current || !slotCanvas.current) return;
     const ctx = canvas.current.getContext('2d');
     ctx?.save();
     ctx?.scale(window.devicePixelRatio, window.devicePixelRatio);
     setCanvasCtx(ctx);
 
+    const sCtx = slotCanvas.current?.getContext('2d');
+    sCtx?.save();
+    sCtx?.scale(window.devicePixelRatio, window.devicePixelRatio);
+    setSlotCtx(sCtx);
+
     return () => {
       ctx?.restore();
-      ctx?.clearRect(0, 0, size, size)
+      ctx?.clearRect(0, 0, size, size);
       setCanvasCtx(null);
+
+      sCtx?.restore();
+      sCtx?.clearRect(0, 0, size, size);
+      setSlotCtx(null);
     }
   }, [id, size]);
 
   // draw slot
   useEffect(() => {
-    const ctx = canvasCtx;
+    const ctx = slotCtx;
     if (!ctx) return;
     const radius = size / 2;
 
@@ -61,8 +79,27 @@ export default function CircularProgress(props: Props) {
 
     slotPath.current = path;
 
-    return () => { slotPath.current = null };
-  }, [size, color, lineWidth, initVal, canvasCtx]);
+    return () => { 
+      slotPath.current = null;
+      slotCtx?.clearRect(0, 0, size, size);
+    };
+  }, [size, color, lineWidth, initVal, slotCtx]);
+
+    // draw ring
+    useEffect(() => {
+      const ctx = canvasCtx;
+      if (!ctx) return;
+      const radius = size / 2;
+  
+      ctx.lineWidth = lineWidth;
+      ctx.lineCap = 'round';
+      ctx.strokeStyle = color;
+  
+      const path = new Path2D();
+      path.arc(radius, radius, radius - lineWidth, -0.5 * Math.PI, initVal - 0.5 * Math.PI);
+      ctx.stroke(path);
+  
+    }, [canvasCtx, color, initVal, lineWidth, size, status]);
 
   // draw button
   useEffect(() => {
@@ -89,9 +126,6 @@ export default function CircularProgress(props: Props) {
       canvasCtx?.clearRect(0, 0, size, size);
     };
   }, [canvasCtx, color, initVal, lineWidth, size, status]);
-
-  // const drawRing = (ctx: CanvasRenderingContext2D) => {
-  // };
 
   // const test = (x: number, y: number) => {
   //   const path = new Path2D();
@@ -127,24 +161,38 @@ export default function CircularProgress(props: Props) {
   }
 
   const pointInSlot = (x: number, y: number): boolean => {
-    if (!canvasCtx || !slotPath.current) return false;
-    return canvasCtx.isPointInStroke(slotPath.current, x , y);
+    if (!slotCtx || !slotPath.current) return false;
+    return slotCtx.isPointInStroke(slotPath.current, x , y);
   }
 
   const calculateAngle = (x: number, y: number): number => {
     const {x: tx , y: ty} = transletePoint(x, y);
     const dot = tx * 0 + ty * (-1);
     const det = tx * (-1) - ty * 0;
-    return -Math.atan2(det, dot);
+    const angel =  (- Math.atan2(det, dot) + 2 * Math.PI) % (2 * Math.PI);
+    return angel;
+    // return dragging.current ? Math.abs(angel - initVal) <= s ? angel : initVal : angel;
+  }
+
+  const updateInitVal = (val: number) => {
+    val = parseFloat(val.toFixed(2));
+    val = val === 6.27 ? 6.28 : val === 0.01 ? 0 : val;
+    if (val !== initVal && ((dragging.current && Math.abs(val - initVal) <= 0.1) || !dragging.current)) {
+      console.log('udateVal', val);
+      setInitVal(val)
+      if (props.onInitValChange) {
+        props.onInitValChange(val);
+      }
+    }
   }
 
   const handleMouseMove = (e: React.MouseEvent): void => {
+    if (!canvas.current || status !== 'setting') return;
     let {x, y} = getCoordination(e);
-    if (!canvas.current) return;
 
     if (dragging.current) {
       // redraw
-      setInitVal(calculateAngle(x, y));
+      updateInitVal(calculateAngle(x, y))
     } else {
       if (pointInButton(x, y)) {
         canvas.current.style.cursor = 'pointer';
@@ -161,7 +209,7 @@ export default function CircularProgress(props: Props) {
       dragging.current = true;
     } else if (pointInSlot(x, y)) {
       // redraw the bar
-      setInitVal(calculateAngle(x, y))
+      updateInitVal(calculateAngle(x, y));
     }
   }
 
@@ -170,15 +218,32 @@ export default function CircularProgress(props: Props) {
   }
 
   return (
-    <canvas
-      id={id}
-      width={size * window.devicePixelRatio}
-      height={size * window.devicePixelRatio}
-      style={{width: '100%', ...props.style}}
-      onMouseMove={handleMouseMove}
-      onMouseDown={handleMouseDown}
-      onMouseUp={handleMouseUp}
-    >
-    </canvas>
+    <>
+      <canvas
+        id={slotId}
+        width={size * window.devicePixelRatio}
+        height={size * window.devicePixelRatio}
+        style={{
+          width: size,
+          height: size,
+          ...props.style
+        }}
+      >
+      </canvas>
+      <canvas
+        id={id}
+        width={size * window.devicePixelRatio}
+        height={size * window.devicePixelRatio}
+        style={{
+          width: size,
+          height: size,
+          ...props.style
+        }}
+        onMouseMove={handleMouseMove}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+      >
+      </canvas>
+    </>
   );
 }
--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,37 +1,81 @@
+import { PlayCircleFilledOutlined, StopCircleOutlined } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
 import React, { useEffect, useRef, useState } from 'react';
 import CircularProgress, { Status } from './CircularProgress/CircularProgress';
 
 export default function Clock() {
   const [date, setDate] = useState<Date>(new Date());
   const [status, setStatus] = useState<Status>('setting');
+  const [isTicking, setIsTicking] = useState<boolean>(true);
+  const [progressAngel, setProgressAngel] = useState<number>(0);
+
   const timerID = useRef<undefined | NodeJS.Timer>();
+  const curAngel = useRef<number>(0);
 
   const tick = (): void => {
-    setDate(new Date());
+    if (status === 'counting') {
+      const countDown = new Date(date);
+      countDown.setTime(date.getTime() - 1000);
+      setDate(countDown);
+    } else {
+      setDate(new Date());
+    }
   }
 
   const onStatusChange = (s: Status) => {
     setStatus(s);
   }
 
+  const setTime = (angel: number) => {
+    curAngel.current = angel;
+
+    if (isTicking) {
+      setIsTicking(false);
+    }
+    let secs = 10800;
+    const near2Pi = 6.28;
+    if (angel <= near2Pi) {
+      secs *= angel / near2Pi;
+    }
+    const countDown = new Date();
+    countDown.setHours(Math.floor(secs / 3600), Math.floor(secs % 3600 / 60), secs % 60);
+    console.log('[setTime]', countDown.toLocaleTimeString());
+    setDate(countDown);
+  }
+
   useEffect(() => {
-    timerID.current = setInterval(
-      () => tick(),
-      1000
-    );
-    
+    if (isTicking) {
+      timerID.current = setInterval(
+        () => tick(),
+        1000
+      );
+    }
     return () => {
       clearInterval(timerID.current);
       timerID.current = undefined;
     }
-  }, [])
+  }, [isTicking, tick]);
+
+  const startCountDown = () => {
+    setStatus('counting');
+    setIsTicking(true);
+    setProgressAngel(curAngel.current);
+  };
+
+  const stopCountDown = () => {
+    setStatus('setting');
+    setProgressAngel(0);
+    // setIsTicking(false);
+  };
 
   return (
     <div>
       <CircularProgress
-        size={400}
+        size={500}
         status={status}
+        initVal={progressAngel}
         onStatusChange={onStatusChange}
+        onInitValChange={setTime}
         style={{
           position: 'absolute',
           top: '50%',
@@ -48,6 +92,15 @@ export default function Clock() {
       >
         { date.toLocaleTimeString() }
       </h1>
+      {
+        status === 'setting' &&
+        <IconButton color="primary" onClick={startCountDown}>
+          <PlayCircleFilledOutlined />
+        </IconButton>
+      }
+      <IconButton color="primary" onClick={stopCountDown}>
+        <StopCircleOutlined />
+      </IconButton>
     </div>
   )
 };
